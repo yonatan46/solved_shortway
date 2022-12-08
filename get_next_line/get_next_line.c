1875cbeb3620aadbff022398907667392d5a9237
yonatan46 <yonatankefyalew65@gmail.com>
2022-12-08 13:43:49 +0400

    it's even shorter now

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,45 +1,31 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<fcntl.h>
-// #define BUFFER_SIZE 1
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+// # define BUFFER_SIZE 1 
+
 char *get_next_line(int fd)
 {
-	if (BUFFER_SIZE <= 0 || fd < 0)
-		return(NULL);
-	char store[100000];
-	char buf;
-	int rd = 1;
-	int y = 0;
-	store[0] = '\0';
-	while (rd)
-	{
-		rd = read(fd, &buf, 1);
-		if (rd == 0)
-			break ;
-		store[y] = buf;
-		store[y + 1] = '\0';
-		if (buf == '\n')
-			break;		
-		y++;
-	}
-	if (store[0] == '\0')
-		return(NULL);	
-	int x = 0;
-	char *copy;
-	while (store[x++]);
-	copy = malloc(sizeof(char) * x + 1);
-	x = -1;
-	while (store[++x])
-		copy[x] = store[x];
-	copy[x] = '\0';
-	return(copy);	
+    if (BUFFER_SIZE <= 0 || fd < 0) return(NULL);
+    char *copy, *tmp;
+    copy = calloc(sizeof(char), 100000);  
+    tmp = copy;
+    while (read(fd, tmp, 1)) if (*(tmp++) == '\n') break;
+    return (copy[0] == '\0' ? NULL: copy);
 }
 
 // int main()
 // {
-// 	int fd = open("new", O_RDONLY);
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	// printf("%s", ft_strdup("hello\n"));
+//     int fd = open("new", O_RDONLY);
+//     char *s = get_next_line(fd);
+//     // while (s)
+//     // {
+//     //     printf("%s", s);
+//     //     s = get_next_line(fd);
+//     // }
+//     // s = get_next_line(fd);
+//     printf("%s", s);
+//     s = get_next_line(fd);
+//     printf("%s", s);
 // }
